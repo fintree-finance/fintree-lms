fca32f9810c9ab91d728d727bad7d1baa4829e66
fintree-finance <fintreefinance19@gmail.com>
2025-07-23 17:37:08 +0530

    env file

--- backend/config/db.js
+++ backend/config/db.js
@@ -21,6 +21,7 @@ const mysql = require('mysql2');
  
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || 'Fintree@2024',
     database: process.env.DB_NAME || 'fintree_lms',
